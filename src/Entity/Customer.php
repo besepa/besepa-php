b8f73b85bfc379250a682c9dca5af60f4159c28c
Asier Marqués <asiermarques@gmail.com>
2016-09-29 10:58:56 +0200

    complete customer fields

--- src/Entity/Customer.php
+++ src/Entity/Customer.php
@@ -10,6 +10,28 @@ class Customer implements EntityInterface{
 
 	public $taxid;
 
-    public $ref;
+    public $reference;
+
+    public $contact_name;
+
+    public $contact_email;
+
+    public $contact_phone;
+
+    public $address_street;
+
+    public $address_city;
+
+    public $address_postalcode;
+
+    public $address_state;
+
+    public $address_country;
+
+    public $status;
+
+    public $created_at;
+
+
 
 }
